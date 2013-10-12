55368a70b4695bd721ff974873c57acbb74a0e12
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2013-10-12 10:37:00 -0400

    changes partials

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.new({ email: 'juanc.com@gmail.com',
+                  password: 'demo1234',
+                  username: 'admin',
+                  first_name: 'juanca',
+                  last_name: 'condori'
+                })
+user.save
+user.add_role :admin
