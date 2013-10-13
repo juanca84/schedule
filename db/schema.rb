21ce3086d2f1cf90ca998185e29ba93f4f6a05d0
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2013-10-13 17:12:53 -0400

    change database postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130912032330) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lessons", force: true do |t|
     t.datetime "hour_start"
     t.datetime "hour_finish"
