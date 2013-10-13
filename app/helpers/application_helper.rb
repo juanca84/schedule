d9589494413dd860f044502b7fae37f8ba3c8a2b
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2013-10-13 18:52:28 -0400

    changes in nav_bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def get_page_id
+    if current_user
+      base = controller.class.to_s.gsub("Controller", '').underscore.gsub("/", '_')
+    end
+    return @page_id || "#{base}_#{controller.action_name}"
+  end
 end
